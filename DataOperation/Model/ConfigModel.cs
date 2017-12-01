4e1e289c3cd4c7f1cc24d85cd9c04fbe2172053d
Zhang Guotao <zhangguotao@outlook.com>
2017-12-01 15:31:16 +0800

    配置文件对象

---
+++ DataOperation/Model/ConfigModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataOperation.Model
+{
+    public class ConfigModel
+    {
+
+        /// <summary>
+        /// ID
+        /// </summary>
+        public string CId { get; set; }
+
+        /// <summary>
+        /// 书签文件地址
+        /// </summary>
+        public string FilePath { get; set; }
+        /// <summary>
+        /// 书签类型
+        /// </summary>
+        public string Type { get; set; }
+    }
+}
