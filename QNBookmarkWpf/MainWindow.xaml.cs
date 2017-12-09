d0eba364981ac08d42d4de145b618231ea4d2e77
Zhang Guotao <zhangguotao@outlook.com>
2017-12-09 20:56:01 +0800

    Wpf项目调整

--- QNBookmarkWpf/MainWindow.xaml.cs
+++ QNBookmarkWpf/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataOperation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DataOperation.Model;
 
 namespace QNBookmarkWpf
 {
@@ -22,6 +24,30 @@ namespace QNBookmarkWpf
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private List<MyBookmarks> _myBookmarkses;
+        private void Test_OnClick(object sender, RoutedEventArgs e)
+        {
+            string filePath = @"C:\Users\z8489\Desktop\Bookmarks";
+            string str = StringConvert.FileRead(filePath);
+            ChromeBookmarks chromeBookmarks = (ChromeBookmarks)StringConvert.JsonToList<ChromeBookmarks>(str);
+            BookmarksData bookmarksData = new BookmarksData();
+            _myBookmarkses = bookmarksData.GetBookmarkses(chromeBookmarks);
+            ConteView.ItemsSource = _myBookmarkses;
+        }
+
+        private void MenuVisit_OnClick(object sender, RoutedEventArgs e)
+        {
+            MyBookmarks myBookmarks = (MyBookmarks) ConteView.SelectedItems[0];
+
+            System.Diagnostics.Process.Start(myBookmarks.Url);
+        }
+
+        private void Setting_OnClick(object sender, RoutedEventArgs e)
+        {
+            QNBookmarkWpf.Setting setting=new Setting();
+            setting.ShowDialog();
         }
     }
 }
---
+++ QNBookmarkWpf/Setting.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QNBookmarkWpf
+{
+    /// <summary>
+    /// Setting.xaml 的交互逻辑
+    /// </summary>
+    public partial class Setting : Window
+    {
+        public Setting()
+        {
+            InitializeComponent();
+        }
+
+        private void FilePath_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+            MessageBox.Show("选择文件");
+            filePath.Text = "文件地址是.............";
+        }
+    }
+}
