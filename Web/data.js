fec21f074e7d906b96c4458aa3953d6885072da4
Zhang Guotao <zhangguotao@outlook.com>
2017-12-10 13:29:51 +0800

    添加网页

---
+++ Web/data.js
@@ -0,0 +1,124 @@
+var InfoData = {
+    "Type": [
+        "下载资源",
+        "工作用组",
+        "代码托管",
+        "帆软"
+    ],
+    "Info": [
+        {
+            "Type": "下载资源",
+            "Name": "网易云音乐 听见好时光",
+            "Url": "http://music.163.com/#/outchain/3/15869133/m/how",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "教委年报审核",
+            "Url": "http://app.cwc.edu.sh.cn:8000/ssoJW/Auditing/Index",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "健康FineBI",
+            "Url": "http://newoa.sumhs.edu.cn:8080/FineBI/ReportServer",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "月报表填报期间设置",
+            "Url": "http://app.cwc.edu.sh.cn:8000/SHECReport/Batch",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "MyApps|OBPM 我的管理平台 - Teemlink.com",
+            "Url": "http://app.cwc.edu.sh.cn:8080/fs/admin/",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "教委月报审核",
+            "Url": "http://app.cwc.edu.sh.cn:8000/SHECReport/DepartBatch/Audit",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "上海教委财务信息监管平台",
+            "Url": "http://fs.cwc.edu.sh.cn:8001/FinanceMonitor",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "策略管理",
+            "Url": "http://fs.cwc.edu.sh.cn:83/TacticsManage.aspx",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "教委FineReport",
+            "Url": "http://app.cwc.edu.sh.cn:8080/WebReport/ReportServer",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "下载资源",
+            "Name": "前置机管理",
+            "Url": "http://app.cwc.edu.sh.cn:8000/SeaskyFIDataWeb",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "前置机在线状态",
+            "Url": "http://app.cwc.edu.sh.cn:8000/SeaskyFIDataWeb/Home/OnlineStatusView",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "教委帆软登录",
+            "Url": "http://app.cwc.edu.sh.cn:8080/WebReport/ReportServer?op=fs_load&cmd=fs_signin&_=1484181771073",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "民办前置机老版查询",
+            "Url": "http://fs.cwc.edu.sh.cn:82/CompanyMonitor.aspx",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "工作用组",
+            "Name": "健康新版BIFineBI 数据决策系统",
+            "Url": "http://bi.sumhs.edu.cn/FineBI/ReportServer?op=fs",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "帆软",
+            "Name": "Excel导出-帆软论坛",
+            "Url": "http://bbs.finereport.com/thread-64923-1-1.html",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "帆软",
+            "Name": "分页预览时自动跳转到下一页（大屏展示数据自动翻页）-帆软论坛",
+            "Url": "http://bbs.finereport.com/thread-69991-1-1.html",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "帆软",
+            "Name": "FineReport帮助文档",
+            "Url": "http://help.finereport.com/",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "帆软",
+            "Name": "帆软应用中心",
+            "Url": "http://shop.finereport.com/",
+            "Source": "Chrome"
+        },
+        {
+            "Type": "帆软",
+            "Name": "【新提醒】写给帆软插件开发者-帆软论坛",
+            "Url": "http://bbs.fanruan.com/forum.php?mod=viewthread&tid=69365&fromuid=70447",
+            "Source": "Chrome"
+        }
+    ]
+}
